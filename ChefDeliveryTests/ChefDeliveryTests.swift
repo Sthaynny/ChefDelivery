4b23fc094aaa99516e9645b4e9ee81e51052e71b
Igor Sthaynny <46109133+Sthaynny@users.noreply.github.com>
2025-01-24 15:35:21 -0300

    feat: add projeto Alura estudos swift

---
+++ ChefDeliveryTests/ChefDeliveryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryTests.swift
+//  ChefDeliveryTests
+//
+//  Created by Igor Costa Nascimento on 24/01/25.
+//
+
+import Testing
+@testable import ChefDelivery
+
+struct ChefDeliveryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
