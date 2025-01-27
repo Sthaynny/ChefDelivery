62b1fdd306f6b6833f2835d344509c74bd341bf0
sthaynnyntt <igor.costanascimento@nttdata.com>
2025-01-27 16:05:17 -0300

    feat: criando tela de pruduto

---
+++ ChefDelivery/ProductView/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  ChefDelivery
+//
+//  Created by Igor Costa Nascimento on 27/01/25.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
--- ChefDelivery/StoresView/StoreDetailsView.swift
+++ ChefDelivery/StoresView/StoreDetailsView.swift
@@ -45,24 +45,32 @@ struct StoreDetailsView: View {
                     .padding()
                 ForEach(store.products){
                     product in
-                    HStack(spacing: 8){
-                        VStack(alignment: .leading, spacing: 8){
-                            Text(product.name)
-                                .bold()
-                            Text(product.description)
-                                .foregroundColor(.black.opacity(0.5))
-                            
-                            Text(product.formatPrice)
-                                .bold()
-                        }
-                        Spacer()
-                        Image(product.image)
-                            .resizable()
-                            .scaledToFit()
-                            .cornerRadius(12)
-                            .frame(width: 120, height: 120)
-                            .shadow(color: .black.opacity(0.3),radius: 20, x: 6 ,y: 8)
-                    }.padding()
+                    
+                    NavigationLink {
+                        ProductDetailView()
+                    } label: {
+                        HStack(spacing: 8){
+                            VStack(alignment: .leading, spacing: 8){
+                                Text(product.name)
+                                    .bold()
+                                Text(product.description)
+                                    .foregroundColor(.black.opacity(0.5))
+                                
+                                Text(product.formatPrice)
+                                    .bold()
+                            }
+                            Spacer()
+                            Image(product.image)
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(12)
+                                .frame(width: 120, height: 120)
+                                .shadow(color: .black.opacity(0.3),radius: 20, x: 6 ,y: 8)
+                        }.padding()
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.leading)
+                    }
+                   
                 }
                 
             }
